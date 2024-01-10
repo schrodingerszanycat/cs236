da2bcdc4df3da4b9dad38fbfc1e938214572360f
schrodingerszanycat <121005817+schrodingerszanycat@users.noreply.github.com>
2024-01-10 05:28:49 +0000

    Make changes

--- 2.py
+++ 2.py
@@ -1,7 +1,17 @@
+def print_unique(lst):
+    elements = set()
+    for x in lst:
+        elements.add(x)
+    print("The unique entries are: ")
+    for element in elements:
+        print(element)
+
 lst = []
 n = int(input("Enter number of elements : "))
 
 for i in range(0, n):
     ele = int(input())
     lst.append(ele)  
+
+print_unique(lst)
  
