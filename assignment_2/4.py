a5adfffa112514e04effcc345f2fdd058cde82d8
schrodingerszanycat <121005817+schrodingerszanycat@users.noreply.github.com>
2024-01-17 05:51:28 +0000

    Add

--- assignment_2/4.py
+++ assignment_2/4.py
@@ -4,13 +4,10 @@ def main():
     with open("tmp.txt","r+") as file:
         data = file.read()
         lines = data.split()
-        for i in range(len(lines)):
-            print(lines[i])
-            
+        print(lines)
         for word in lines:
             if not word.isnumeric(): # counts only words, not numbers
                 count += 1
-    
     print("Count:", count)
 
     # Copying file
@@ -19,10 +16,19 @@ def main():
         data = file.readlines()
         for i in range(len(data)):
             file_copy.write(data[i])
-    print("Contents of file successfully copied!")
+    print("Contents of file successfully copied!\n")
 
-    
-
+    # Searching for a specific pattern 
+    pattern = input("Enter pattern...")
+    with open("tmp.txt","r+") as file:
+        data = file.read()
+        lines = data.split()
+        #print(lines)
+        for i in range(len(lines)):
+            if pattern.upper() in lines[i].upper():
+                print("Pattern found!")
+                exit()
+    print("Pattern not found.\n")
 
 if __name__=="__main__": 
     main() 
