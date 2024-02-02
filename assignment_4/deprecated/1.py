aa73caeda0b0962ed4fd891aa0c0bee71abcf726
schrodingerszanycat <152669825+thebardofavon@users.noreply.github.com>
2024-02-02 22:06:07 +0530

    Commit changes

--- assignment_4/deprecated/1.py
+++ assignment_4/deprecated/1.py
@@ -42,6 +42,27 @@ class EightQ:
                 queue.put(queens)
         return solutions
 
+    # def solve_iddfs(self):
+    #     solutions = []
+    #     for i in range(self.size):
+    #         stack = [[]]
+    #         while stack:
+    #             solution = stack.pop()
+    #             if self.isNotSafe(solution):
+    #                 continue
+    #             row = len(solution)
+    #             if row == self.size:
+    #                 solutions.append(solution)
+    #                 continue
+    #             for col in range(self.size):
+    #                 queen = (row, col)
+    #                 queens = solution.copy()
+    #                 queens.append(queen)
+    #                 stack.append(queens)
+    #     res = []
+    #     [res.append(x) for x in solutions if x not in res]
+    #     return res
+
     def isNotSafe(self, queens):
         for i in range(1, len(queens)):
             for j in range(0, i):
@@ -61,11 +82,11 @@ class EightQ:
         print(' ---' * self.size)
 
 def main():
-
     eight_queens = EightQ()
 
     dfs_solutions = eight_queens.solve_dfs()
     bfs_solutions = eight_queens.solve_bfs()
+    iddfs_solutions = eight_queens.solve_iddfs()
 
     for i, solution in enumerate(dfs_solutions):
         print('DFS Solution %d:' % (i + 1))
@@ -75,8 +96,13 @@ def main():
         print('BFS Solution %d:' % (i + 1))
         eight_queens.print(solution)
 
+    for i, solution in enumerate(iddfs_solutions):
+        print('IDDFS Solution %d:' % (i + 1))
+        eight_queens.print(solution)
+
     print('Total DFS solutions: %d' % len(dfs_solutions))
     print('Total BFS solutions: %d' % len(bfs_solutions))
+    print('Total IDDFS solutions: %d' % len(iddfs_solutions))
 
 if __name__ == '__main__':
     main()
